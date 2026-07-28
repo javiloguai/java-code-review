4923ac6f5620e1f22a83be3d56f3acd73f5e1f00
javiloguai <javiloguai.xaic@gmail.com>
2026-07-28 21:18:06 +0200

    Changin name of COlumn with underscore to follow Spring convetions; jpa is failing trying to retrieve it because is looking for underscores without a column specification in the entity

--- review/src/main/java/schwarz/jobs/interview/coupon/core/domain/CouponEntity.java
+++ review/src/main/java/schwarz/jobs/interview/coupon/core/domain/CouponEntity.java
@@ -29,7 +29,7 @@ public class CouponEntity {
     @Column(name = "discount", precision = 10, scale = 2)
     private BigDecimal discount;
 
-    @Column(name = "minBasketValue", precision = 10, scale = 2)
+    @Column(name = "min_basket_value", precision = 10, scale = 2)
     private BigDecimal minBasketValue;
 
 }
--- review/src/main/resources/data.sql
+++ review/src/main/resources/data.sql
@@ -4,10 +4,10 @@ CREATE TABLE coupons (
   id INT AUTO_INCREMENT  PRIMARY KEY,
   code  VARCHAR(250) NOT NULL,
   discount NUMBER(10,2) NOT NULL,
-  minBasketValue NUMBER(10,2) DEFAULT NULL
+  min_basket_value NUMBER(10,2) DEFAULT NULL
 );
  
-INSERT INTO coupons (code, discount, minBasketValue) VALUES
+INSERT INTO coupons (code, discount, min_basket_value) VALUES
   ('TEST1', 10.00, 50.00),
   ('TEST2', 15.00, 100.00),
   ('TEST3', 20.00, 200.00);
