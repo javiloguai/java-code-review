30a156e34133af04d925b8991eb7b2e8fa93d90f
javiloguai <javiloguai.xaic@gmail.com>
2026-07-29 02:35:10 +0200

    -Id sequence generator strategy missing for Id in Coiupon Entity - CouponrequestDTO needs Empty Constructor to be deserialized by JSON

--- review/src/main/java/schwarz/jobs/interview/coupon/web/dto/CouponRequestDTO.java
+++ review/src/main/java/schwarz/jobs/interview/coupon/web/dto/CouponRequestDTO.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 import javax.validation.constraints.NotNull;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CouponRequestDTO {
 
     @NotNull
